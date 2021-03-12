e394ff293c2101b5b99191cae3476c6d3b33cee3
kitarira <63104268+kitarira@users.noreply.github.com>
2021-03-12 21:23:02 +0900

    Add files via upload

---
+++ practice/find_alpha.py
@@ -0,0 +1,20 @@
+import random
+import datetime
+
+ALP = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+
+r = random.choice(ALP)
+alp = ""
+for i in ALP:
+    if i != r:
+        alp = alp + i
+print(alp)
+st = datetime.datetime.now() 
+
+ans = input("抜けているアルファベットは？　（大文字で入力してください）")
+if ans == r:
+    print("正解です")
+    et = datetime.datetime.now()
+    print(str((et-st).microseconds / 100000) + "秒かかりました")
+else:
+    print("違います")
